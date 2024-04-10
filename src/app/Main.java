a4b95f7270a20167b43fb27d39825e1e4cdcc7e1
조명근 <whaudrms123@gmail.com>
2024-04-10 18:20:34 +0900

    메인 메뉴 틀

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,42 @@
 package app;
 
+import java.sql.Connection;
+import java.util.Scanner;
+
 public class Main {
+	private Scanner scanner = new Scanner(System.in);
+	private Connection conn;
+	
+	public void home() {
+		System.out.println("hello world");
+		
+		System.out.println();
+		System.out.println("<Sign in | Sign up>");
+		System.out.println("-----------------------------------------------------------------------");
+		System.out.println("선택: 1.로그인 | 2.회원가입 ");
+		System.out.println("선택");
+		String menuNo = scanner.nextLine();
+		System.out.println();
+		
+		switch(menuNo) {
+			case "1" -> login();
+			case "2" -> create_account();
+		}
+	}
+	public void login() {
+		System.out.println("로그인");
+	}
+	
+	public void create_account() {
+		System.out.println("회원가입");
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		// presentation layer
+		Main main = new Main();
+		main.home();
 	}
 
 }
