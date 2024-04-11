5d430444ec9d449863957e5956e0a7f26e4c2114
chomyeonggeun <lightkeun@gmail.com>
2024-04-11 15:56:04 +0900

    예약하기 기능

--- src/app/SeatType.java
+++ src/app/SeatType.java
@@ -1,5 +1,11 @@
 package app;
 
-public class SeatType {
+import lombok.Data;
 
+@Data
+public class SeatType {
+	private int seat_no;
+	private String type;
+	private int num;
+	private String check_flag;
 }
